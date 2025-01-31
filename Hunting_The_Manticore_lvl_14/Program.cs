403db93add462835b9668934bea5bdbf6c18d170
HaydenHartmann1 <156471807+HaydenHartmann1@users.noreply.github.com>
2025-01-31 13:08:22 -0600

    done
    done

---
+++ Hunting_The_Manticore_lvl_14/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+    Hayden Hartmann
+    1/29/2025
+    Hunting The Manticore : And Practice
+*/
+
+namespace Hunting_The_Manticore_lvl_14
+{
+    public class BossBattle
+    {
+        public static void Main(string[] args)
+        {
+            Delete delete = new Delete("hayden", 5, 8);
+
+            Console.WriteLine(delete.GetCount);
+            
+            Console.WriteLine(delete.Num1);
+
+            delete.Num1 = -1;
+
+            Console.WriteLine(delete.Num1);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Hunting_The_Manticore_lvl_14/delete.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Hunting_The_Manticore_lvl_14
+{
+    public class Delete
+    {
+
+        private int num1 = int.MaxValue;
+        private double num2 = double.MaxValue;
+        private string name = "n/a";
+        private static int count = 0;
+
+        public Delete(string name, int num1, double num2)
+        {
+            this.name = name;
+            this.num1 = num1;
+            this.num2 = num2;
+            count++;
+        }
+
+        public int GetCount
+        {
+            get { return count; }
+        }
+        public int Num1
+        {
+            get { return this.num1; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Value must be > 0");
+                }
+                else
+                {
+                    this.num1 = value;
+                }
+            }
+        }
+
+
+    }
+}
